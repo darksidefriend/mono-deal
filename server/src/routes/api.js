09a409b3741f9404f4fc0768e0da38c046f9f334
Daniil Savelev <dansavels@yandex.ru>
2026-02-04 17:28:21 +0300

    delete project

--- server/src/routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Простой маршрут для проверки работы API
-router.get('/health', (req, res) => {
-  res.json({ status: 'OK', message: 'Server is running' });
-});
-
-module.exports = router;
